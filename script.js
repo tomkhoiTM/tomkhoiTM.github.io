f900abc64ca5a71a31716dda69d0c69ab1cde45e
tomkhoiTM <98985416+tomkhoiTM@users.noreply.github.com>
2024-09-21 00:07:25 -0700

    Webpage Total Overhaul (v.1.0)
    Update 1.0 is here with new interface, updated with more pictures and new experiences.
    
    - Added new CSS styles and themes and a new favicon.
    - Updated site content and redirected links.
    - Updated CV, featured media (photos, videos, and other files), and portfolio.

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-function onLoad() {
-  var themeSelector = document.getElementById('themeSelector');
-  for (var themeName in themeMap) {
-    var opt = document.createElement('option');
-    opt.value = themeName;
-    opt.innerHTML = capitalizeFirstLetter(themeName);
-    themeSelector.appendChild(opt);
-  }
-  if (localStorage.getItem('theme') != null) {
-    themeSelector.value = localStorage.getItem('theme');
-    toggleTheme();
-  }
+// Collapsible event
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    console.log(content);
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
 
-function toggleTheme() {
-  var themeName = themeSelector.value;
-  localStorage.setItem('theme', themeName);
-  var element = document.getElementsByTagName('html')[0];
-  changeTheme(element, themeMap[themeName]);
-}
-
-function changeTheme(element, theme) {
-  element.style.setProperty("--primary-background-color", theme['background-color']);
-  element.style.setProperty("--primary-text-color", theme['text-color']);
-}
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+// Title animation remains the same
+let titleCtn = document.getElementById("page-title-ctn");
+let title = document.getElementById("page-title");
+let titleAlt = document.getElementById("page-title-alt");
+titleCtn.addEventListener("mouseover", () => {
+    title.style.opacity = 0;
+    setTimeout(function() {
+        title.style.display = "none";
+        titleAlt.style.display = "block";
+        setTimeout(function() {
+            titleAlt.style.opacity = 1;
+        }, 15)
+    }, 200)
+})
+titleCtn.addEventListener("mouseleave", () => {
+    titleAlt.style.opacity = 0;
+    setTimeout(function() {
+        titleAlt.style.display = "none";
+        title.style.display = "block";
+        setTimeout(function() {
+            title.style.opacity = 1;
+        }, 15)
+    }, 200)
+})
